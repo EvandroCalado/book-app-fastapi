201db76a6577baa36d5f73e6680977eb4e915982
Evandro Calado <evandrocalado07@gmail.com>
2024-10-18 23:35:55 -0300

    chore: create table books on database

--- book_app_fastapi/database.py
+++ book_app_fastapi/database.py
@@ -1,18 +1,15 @@
 from sqlalchemy.ext.asyncio import create_async_engine
-from sqlmodel import text
+from sqlmodel import SQLModel
 
 from book_app_fastapi.settings import settings
 
 engine = create_async_engine(
     url=settings.DATABASE_URL,
+    connect_args={'ssl': True},
     echo=True,
 )
 
 
 async def init_db():
     async with engine.begin() as conn:
-        statement = text("select 'Hello World'")
-
-        result = await conn.execute(statement)
-
-        print(result.all())
+        await conn.run_sync(SQLModel.metadata.create_all)
--- book_app_fastapi/models.py
+++ book_app_fastapi/models.py
@@ -1,11 +1,32 @@
-from pydantic import BaseModel
+from datetime import datetime
+from uuid import UUID, uuid4
+
+import sqlalchemy.dialects.postgresql as pg
+from sqlmodel import Column, Field, SQLModel
 
 
-class Book(BaseModel):
-    id: int
+class Book(SQLModel, table=True):
+    id: UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            unique=True,
+            nullable=False,
+            default=uuid4(),
+        )
+    )
     title: str
     author: str
     publisher: str
     published_date: str
     page_count: int
     language: str
+    created_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP, default=datetime.now)
+    )
+    updated_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP, default=datetime.now)
+    )
+
+    def __repr__(self) -> str:
+        return f'<Book {self.title}>'
